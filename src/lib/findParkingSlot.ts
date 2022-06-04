0c98546bb285d0b7fb85e4d0125f440a385e79f3
Eighty Three <contact@eighty-three.dev>
2022-06-05 04:01:49 +0800

    fix: prioritize smallest size when finding slot
    Previously, it automatically chooses the nearest available slot,
    e.g. if the car size is 0, it selects the empty 2 slot beside it
    instead of finding the nearest 0 slot available first

--- src/lib/findParkingSlot.ts
+++ src/lib/findParkingSlot.ts
@@ -1,20 +1,41 @@
-import { TCarSize, ICoordinates } from '@/types';
+import { TCarSize, ICoordinates, IParkingSlot } from '@/types';
 import { useParkingSlotsStore } from '@/store';
 
+interface IDistance {
+  slot: IParkingSlot | null;
+  distance: number;
+}
+type TDistances = {
+  [K in TCarSize]: IDistance;
+};
 export const findParkingSlot = (size: TCarSize, entry: ICoordinates) => {
   const parkingSlots = useParkingSlotsStore.getState().parkingSlots;
   const filteredParkingSlots = parkingSlots.filter((v) => size <= v.size && !v.parkedCar);
 
-  let shortestDistance = Infinity;
-  let slot = null;
+  const distances: TDistances = {
+    0: { slot: null, distance: Infinity },
+    1: { slot: null, distance: Infinity },
+    2: { slot: null, distance: Infinity },
+  };
   for (let i = 0; i < filteredParkingSlots.length; i++) {
     const currentSlot = filteredParkingSlots[i];
+    const size = currentSlot.size;
     const distance =
       Math.abs(currentSlot.coordinates.row - entry.row) + Math.abs(currentSlot.coordinates.col - entry.col);
-    if (shortestDistance > distance) {
-      shortestDistance = distance;
-      slot = currentSlot;
+
+    if (distances[size].distance > distance) {
+      distances[size].distance = distance;
+      distances[size].slot = currentSlot;
     }
   }
-  return slot;
+
+  let val = size;
+  while (val < 3) {
+    if (distances[val].slot) {
+      return distances[val].slot;
+    }
+    val++;
+  }
+
+  return null;
 };
